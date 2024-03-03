535b23376b09c1cdffe8025dcbffa947c20f6185
serebrus143 <serebrus143@gmail.com>
2024-03-03 20:49:54 +0300

    ДЗ выполнил

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Задача 1");
+        String firstName = "Ivan";
+        String middleName = "Ivanovich";
+        String lastName = "Ivanov";
+        String separator = " ";
+        String fullName = lastName + separator + firstName + separator + middleName;
+        System.out.println("Ф. И. О. сотрудника — " + fullName);
+
+        System.out.println("Задача 2");
+        System.out.println("Данные Ф. И. О. сотрудника для заполнения отчета — " + fullName.toUpperCase());
+
+        System.out.println("Задача 3");
+        fullName = "Иванов Семён Семёнович";
+        String fullNameWithoutYo = fullName.replace('ё', 'e');
+        System.out.println("Данные ФИО сотрудника — " + fullNameWithoutYo);
     }
 }
